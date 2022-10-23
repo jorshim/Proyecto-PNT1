4293f1693ff0b465fc9b2ba8737acf98bf3e6e79
Jorge Rodriguez <115499335+jorshim@users.noreply.github.com>
2022-10-23 16:58:35 -0300

    Clases

---
+++ jar_Proyecto-PNT1.V2/Models/TipoTurno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jar_Proyecto-PNT1.V2.Models
+{
+    public enum TipoTurno  
+    {
+        ConsultaMedica,
+        PracticaMedica
+    }
+}
