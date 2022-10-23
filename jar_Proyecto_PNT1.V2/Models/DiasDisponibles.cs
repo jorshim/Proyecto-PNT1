085a77c64f952b27171448fa796663013e6751ea
Jorge Rodriguez <115499335+jorshim@users.noreply.github.com>
2022-10-23 18:29:58 -0300

    Create DiasDisponibles.cs

---
+++ jar_Proyecto_PNT1.V2/Models/DiasDisponibles.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jar_Proyecto_PNT1.V2.Models
+{
+    public enum DiasDisponibles
+    {
+        
+        Lunes =1,
+        Martes,
+        Miercoles,
+        Jueves,
+        Viernes
+
+            
+    }
+    
+}
