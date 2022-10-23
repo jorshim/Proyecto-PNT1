4365d8f6c128cbeb663ab94ba0ff66513b1d6d4f
Jorge Rodriguez <115499335+jorshim@users.noreply.github.com>
2022-10-23 16:10:37 -0300

    Add files via upload

---
+++ jar_Proyecto-PNT1.V2/Models/TurnoConsultaMedica.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using static jar_Proyecto-PNT1.V2.Models.Validations;
+
+namespace Proyecto-PNT1.V2.Models
+{
+    public class TurnoConsultaMedica : Turno
+    {
+        public int IdMedico { get; set; }
+
+        [Display(Name = "Fecha del turno")]
+        public string FechaConsultaMedica { get; set; }
+
+        //[Required(ErrorMessage = "El campo no puede quedar vacío")]
+        //[Range(90000, 100000000, ErrorMessage = "El documento debe tener al menos 6 caracteres y como máximo 9")]
+        //[RegularExpression("^[0-9]+$", ErrorMessage = "El documento solo debe contener números")]
+        [DniExistsDB]
+        [Display(Name = "Documento del paciente")]
+        public int DocumentoPaciente { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem> MedicosItems { get; set; }
+
+    }
+}
