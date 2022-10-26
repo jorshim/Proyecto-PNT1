4f09436fb45bdd5ff55e6461f46312f0654d9c46
Jorge Rodriguez <115499335+jorshim@users.noreply.github.com>
2022-10-26 19:14:15 -0300

    Add files via upload

---
+++ jar_Proyecto_PNT1.V2/Migrations/20211109224220_jar_Proyecto_PNT1.V2.Context.ClinicaDatabaseV2Context02.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace jar_Proyecto_PNT1.V2.Migrations
+{
+    public partial class jar_Proyecto_PNT1V2ContextClinicaDatabaseV2Context02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ jar_Proyecto_PNT1.V2/Migrations/20211121234315_jar_Proyecto_PNT1.V2.Context.ClinicaDatabaseV2Context03.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace jar_Proyecto_PNT1.V2.Migrations
+{
+    public partial class jar_Proyecto_PNT1V2ContextClinicaDatabaseV2Context03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TurnoConsultaMedica",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdPaciente = table.Column<int>(nullable: false),
+                    DiasDisponibles = table.Column<int>(nullable: false),
+                    HorasDisponibles = table.Column<int>(nullable: false),
+                    IdMedico = table.Column<int>(nullable: false),
+                    FechaConsultaMedica = table.Column<string>(nullable: true),
+                    DocumentoPaciente = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TurnoConsultaMedica", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TurnoConsultaMedica");
+        }
+    }
+}
